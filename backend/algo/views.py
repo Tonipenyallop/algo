da13298e2d8a201e0232521ba9a3a9b07b595b92
Tonipenyallop <kimtesu521@gmail.com>
2022-08-30 18:43:28 +0900

    add:handle register request

--- backend/algo/views.py
+++ backend/algo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+def register(request):
+    print(request)
+
+    return HttpResponse('hello')
