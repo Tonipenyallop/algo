f7e08f87450a8c37c804d956ed3745a147bb5ade
Tonipenyallop <kimtesu521@gmail.com>
2022-08-30 17:38:20 +0900

    install:axios, prettier, react-boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="">Hello</div>
-  )
+    <div className="">
+      <Button variant="primary" className="btn-primary">
+        Click
+      </Button>
+    </div>
+  );
 }
 
 export default App;
