b49c6e32e1e325a24ad47010420e90b7deb62f93
Tonipenyallop <kimtesu521@gmail.com>
2022-09-12 11:39:03 +0900

    add: tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+// import { Button } from "react-bootstrap";
 import Login from "./components/Login";
-
+import "./input.css";
 function App() {
-  // const arr = new Array(5).fill(new Array(5).fill(0));
+  const [update, setUpdate] = useState("");
+
+  const dfs = (matrix: number[][]): boolean => {
+    const [ROWS, COLS] = [matrix.length, matrix[0].length];
+
+    const queue = [[0, 0]];
+    const directions = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+    let visited = [];
+
+    return false;
+  };
+
   const arr = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -24,21 +40,33 @@ function App() {
   arr[4][2] = 1;
   arr[4][4] = 5;
 
+  dfs(arr);
+
   return (
     <div className="">
-      <Button variant="primary" className="btn-primary">
-        Click
-      </Button>
-      {/* <Login /> */}
-      {/* <div className="">{row()}</div> */}
-      {arr.map((e, idx) => {
-        return (
-          <div className=" text-red-600" key={idx}>
-            {e}
-          </div>
-        );
-      })}
-      <button>Create 2d array</button>
+      <button className=" bg-slate-400 rounded-lg mx-2 my-2 px-2 py-2">
+        Create 2d array
+      </button>
+
+      <div className=" flex flex-col items-center justify-center mt-6">
+        {arr.map((e, idx) => {
+          return (
+            <div className="flex" key={idx}>
+              {e.map((el: number, idx: number) => {
+                if (el === 4)
+                  return <div className="flex bg-green-500">{el}</div>;
+                if (el === 1) return <div className="flex bg-black">{el}</div>;
+                if (el === 5)
+                  return <div className="flex bg-yellow-300">{el}</div>;
+                if (el === 3)
+                  return <div className="flex bg-blue-600">{el}</div>;
+                else return <div className="flex">{el}</div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+      {/* <div className="bg-green-500 my-3 text-white text-center">True</div> */}
     </div>
   );
 }
