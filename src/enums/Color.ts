28e6197067d4f29b8ea687d28731575e6adbba50
Tonipenyallop <kimtesu521@gmail.com>
2022-09-22 17:48:41 +0900

    add: two files

---
+++ src/enums/Color.ts
@@ -0,0 +1,6 @@
+enum Color {
+  START_COLOR = "bg-blue-200",
+  GOAL_COLOR = "bg-yellow-200",
+}
+
+export default Color;
---
+++ src/enums/ColorActions.ts
@@ -0,0 +1,9 @@
+enum ColorAction {
+  START = "start",
+  VISITED = "visited",
+  GOAL = "goal",
+  BLOCK = "block",
+  DEFAULT = "default",
+}
+
+export default ColorAction;
