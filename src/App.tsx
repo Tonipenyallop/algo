feb983d375dfdde9fe402b35f0bfcaa59177a80b
Tonipenyallop <kimtesu521@gmail.com>
2022-09-22 17:50:20 +0900

    refactor:App.tsx, add:mouseEnter event

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,12 @@ import Matrix from "./components/Matrix";
 import "./input.css";
 
 import Position from "./interfaces/Position";
+import Color from "./enums/Color";
+import ColorAction from "./enums/ColorActions";
 function App() {
   let [colorFlag, setColorFlag] = useState<boolean>(false);
   let [isInitialRender, setIsInitialRender] = useState<boolean>(true);
   const [arr, setArr] = useState<number[][]>([[]]);
-
-  enum Color {
-    START = "start",
-    VISITED = "visited",
-    GOAL = "goal",
-    BLOCK = "block",
-    DEFAULT = "default",
-  }
 
   const START_POSITION: Position = { row: 14, col: 10 };
   const GOAL_POSITION: Position = { row: 14, col: 20 };
@@ -27,8 +21,8 @@ function App() {
       setArr(arrayCreater());
       setIsInitialRender(false);
       setTimeout(() => {
-        changeColor(START_POSITION, Color.START);
-        changeColor(GOAL_POSITION, Color.GOAL);
+        changeColor(START_POSITION, ColorAction.START);
+        changeColor(GOAL_POSITION, ColorAction.GOAL);
       }, 500);
     }
   }, [colorFlag, isInitialRender]);
@@ -47,10 +41,13 @@ function App() {
     const col = position.col;
 
     const nodeElement = document.getElementById(`${row}_${col}`);
-    if (action === Color.START) nodeElement?.classList.add("bg-blue-200");
-    else if (action === Color.GOAL) nodeElement?.classList.add("bg-yellow-200");
-    else if (action === Color.BLOCK) nodeElement?.classList.add("bg-blue-400");
-    else if (action === Color.VISITED)
+    if (action === ColorAction.START)
+      nodeElement?.classList.add(Color.START_COLOR);
+    else if (action === ColorAction.GOAL)
+      nodeElement?.classList.add(Color.GOAL_COLOR);
+    else if (action === ColorAction.BLOCK)
+      nodeElement?.classList.add("bg-blue-400");
+    else if (action === ColorAction.VISITED)
       nodeElement?.classList.add("bg-green-400");
     else nodeElement?.classList.add("bg-red-300");
     setColorFlag(!colorFlag);
--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
+import Color from "../enums/Color";
 type Props = {
   row: number;
   col: number;
 };
 
 export default function Node({ row, col }: Props) {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  function changeNodeColor(): void {
+    const node = document.getElementById(`${row}_${col}`);
+    if (
+      node?.classList.contains(Color.START_COLOR) ||
+      node?.classList.contains(Color.GOAL_COLOR)
+    )
+      return;
+    node?.classList.add("bg-black");
+  }
+
   return (
-    <div
-      className="flex justify-center items-center "
-      // onClick={() => console.log("he")}
-    >
+    <div className="flex justify-center items-center ">
       <div
         id={`${row}_${col}`}
         className="w-7 h-7 border-2 duration-1000 flex"
+        // onClick={changeNodeColor}
+        // onMouseDown={changeNodeColor}
+        // onMouseDown={() => {
+        //   setIsClicked(isClicked);
+        //   console.log("hihi");
+        //   console.log(`isClicked Before: ${isClicked}`);
+        //   console.log(`isClicked After: ${isClicked}`);
+        // }}
+        onMouseEnter={changeNodeColor}
       ></div>
     </div>
   );
