775e6573afcaffd1f553abe9c8f5a416933615eb
Tonipenyallop <kimtesu521@gmail.com>
2022-09-22 16:20:53 +0900

    add: matrix component

---
+++ src/components/Matrix.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Node from "./Node";
+interface props {
+  array: number[][];
+}
+
+export default function Matrix({ array }: props) {
+  return (
+    <div className="border-4 flex flex-col flex-wrap h-auto w-auto my-4 ">
+      {array?.map((e: number[], row: number) => {
+        return (
+          <div className="flex" key={row}>
+            {e.map((_: number, col: number) => {
+              return <Node row={row} col={col} key={`${row}_${col}`} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
