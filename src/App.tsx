d7f4a9ba77ede6da0789c2eca5fc0aea572c1d35
Tonipenyallop <kimtesu521@gmail.com>
2022-09-22 16:21:37 +0900

    add: animation, update:create 50x50 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 // import { Button } from "react-bootstrap";
 import Login from "./components/Login";
+import Matrix from "./components/Matrix";
 import Node from "./components/Node";
 import "./input.css";
 import bfs from "./methods/bfs";
 function App() {
   let visited: Set<string> = new Set();
+  let [color, setColor] = useState<boolean>(false);
   const [ROWS, COLS] = [4, 4];
-  const arr = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-  ];
-  enum color {
+  // const arr = [
+  //   [0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0],
+  // ];
+  // let arr: number[][] = [[]];
+  const [arr, setArr] = useState<number[][]>([[]]);
+  enum Color {
     START = "start",
     VISITED = "visited",
     GOAL = "goal",
@@ -25,28 +29,39 @@ function App() {
   // dfs(arr);
 
   useEffect(() => {
-    changeColor(0, 0, color.START);
-    // changeColor(3, 3, color.GOAL);
-    // changeColor(2, 2, color.BLOCK);
+    setArr(arrayCreater());
+    // arrayCreater();
+    changeColor(0, 0, Color.START);
+    changeColor(3, 3, Color.GOAL);
+    changeColor(2, 2, Color.BLOCK);
+    changeColor(1, 2, Color.BLOCK);
+    changeColor(4, 2, Color.BLOCK);
+    changeColor(2, 2, Color.BLOCK);
+
+    //
+    // changeColor(3, 2, color.BLOCK);
+    // changeColor(0, 2, color.BLOCK);
     // changeColor(1, 2, color.BLOCK);
     // changeColor(4, 2, color.BLOCK);
     // changeColor(2, 2, color.BLOCK);
+    // changeColor(3, 3, color.GOAL);
+  }, []);
 
-    //
-    changeColor(3, 2, color.BLOCK);
-    changeColor(0, 2, color.BLOCK);
-    changeColor(1, 2, color.BLOCK);
-    changeColor(4, 2, color.BLOCK);
-    changeColor(2, 2, color.BLOCK);
-    changeColor(3, 3, color.GOAL);
-  });
+  const randomNumberCreater = (): number => {
+    return 0;
+  };
+
+  function arrayCreater(): number[][] {
+    let arr: number[][] = new Array(30).fill(new Array(30).fill(0));
+    return arr;
+  }
 
   const changeColor = (row: number, col: number, action: string) => {
     const nodeElement = document.getElementById(`${row}_${col}`);
-    if (action === color.START) nodeElement?.classList.add("bg-blue-200");
-    else if (action === color.GOAL) nodeElement?.classList.add("bg-yellow-200");
-    else if (action === color.BLOCK) nodeElement?.classList.add("bg-blue-400");
-    else if (action === color.VISITED)
+    if (action === Color.START) nodeElement?.classList.add("bg-blue-200");
+    else if (action === Color.GOAL) nodeElement?.classList.add("bg-yellow-200");
+    else if (action === Color.BLOCK) nodeElement?.classList.add("bg-blue-400");
+    else if (action === Color.VISITED)
       nodeElement?.classList.add("bg-green-400");
     else nodeElement?.classList.add("bg-red-300");
   };
@@ -65,15 +80,7 @@ function App() {
       </button>
 
       <div className=" flex flex-col items-center justify-center mt-6">
-        {arr.map((e: number[], row: number) => {
-          return (
-            <div className="flex" key={row}>
-              {e.map((_: number, col: number) => {
-                return <Node row={row} col={col} key={`${row}_${col}`} />;
-              })}
-            </div>
-          );
-        })}
+        <Matrix array={arr}></Matrix>
       </div>
       {/* <div className="bg-green-500 my-3 text-white text-center">True</div> */}
     </div>
--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -5,5 +5,12 @@ type Props = {
 };
 
 export default function Node({ row, col }: Props) {
-  return <div id={`${row}_${col}`} className="w-10 h-10 border-2"></div>;
+  return (
+    <div className="flex justify-center items-center ">
+      <div
+        id={`${row}_${col}`}
+        className="w-10 h-10 border-2 duration-1000 flex"
+      ></div>
+    </div>
+  );
 }
