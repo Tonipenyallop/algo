efd9dd2e8055708ee32842c7ee6335457b9098cc
Tonipenyallop <kimtesu521@gmail.com>
2022-10-17 22:07:56 +0900

    add: fake data

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -35,6 +35,7 @@ export default function Node({ row, col }: Props) {
         // }}
         onMouseEnter={changeNodeColor}
       ></div>
+      <div className="">fake data</div>
     </div>
   );
 }
